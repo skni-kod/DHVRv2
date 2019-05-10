4e05f70070cb61d0479b5bd798d2f5365eab80ea
Paweł Dziedzic <blavikenn@gmail.com>
2019-05-10 15:33:00 +0200

    Create prefabs for enviro, props and some particles, add Kenney Particle Pack

--- DHVRv2/Assets/_Scripts/Utility/ParticleSpawner.cs
+++ DHVRv2/Assets/_Scripts/Utility/ParticleSpawner.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class ParticleSpawner : MonoBehaviour {
     public ParticleSystem m_ParticleSystem;
+    public bool inheritRotation;
 
     public void Spawn() {
-        var obj = Instantiate(m_ParticleSystem, transform.position, transform.rotation);
+        Quaternion rot = Quaternion.identity;
+        if (inheritRotation) {
+            rot = transform.rotation;
+        }
+        var obj = Instantiate(m_ParticleSystem, transform.position, rot);
         Destroy(obj.gameObject, m_ParticleSystem.main.duration);
     }
 }
