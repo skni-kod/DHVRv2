9ec15272a37503ae1e9fe686552cd1aeb98615f4
Paweł Dziedzic <blavikenn@gmail.com>
2019-05-10 15:36:41 +0200

    Just add Rocket Launcher

---
+++ DHVRv2/Assets/_Scripts/Guns/Rocket.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Rocket : MonoBehaviour {
+    public float _explosionRadius;
+    public float _explosionTime;
+    public float _rocketSpeed;
+
+    public UnityEngine.Events.UnityEvent OnExplode;
+
+    Rigidbody _body;
+
+    private void Awake() {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    void Start() {
+        Invoke("Explode", _explosionTime);
+        _body.velocity = transform.forward * _rocketSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Explode();
+    }
+
+    void Explode() {
+        OnExplode.Invoke();
+
+        var colliders = Physics.OverlapSphere(transform.position, _explosionRadius);
+        foreach (var collider in colliders) {
+            var damageable = collider.GetComponent<Damageable>();
+            if (damageable) {
+                damageable.Damage(1);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ DHVRv2/Assets/_Scripts/Guns/RocketLauncher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncher : Gun
+{
+    public Rocket _rocketPrefab;
+
+    public override void Fire()
+    {
+        var rocket = Instantiate(_rocketPrefab, _gunTip.position, _gunTip.rotation);
+    }
+
+    protected override IEnumerator HandleEffects()
+    {
+        yield return null;
+    }
+}
