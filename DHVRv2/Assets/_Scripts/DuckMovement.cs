e1b8796d49453a1947cd0f6d83e4ac25bc4c9052
Suzurri <44876347+Suzurri@users.noreply.github.com>
2019-05-10 21:35:35 +0200

    Duck Path

---
+++ DHVRv2/Assets/_Scripts/DuckMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class DuckMovement : MonoBehaviour
+{
+
+
+    public EndOfPathInstruction endOfPathInstruction;
+    public VertexPath path;
+    float distanceTravelled;
+    int speed;
+    void Start()
+    {
+        speed = Random.Range(5, 10);
+    }
+
+    void Update()
+    {
+        if (path != null)
+        {
+            distanceTravelled += speed * Time.deltaTime;
+            transform.position = path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+            transform.rotation = path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+        }
+    }
+}
---
+++ DHVRv2/Assets/_Scripts/DuckSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+public class DuckSpawner : MonoBehaviour
+{
+    public GameObject ducky;
+    public VertexPath path;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var positionArray = new Vector3[4];
+        positionArray[0] = new Vector3(Random.Range(-5f, 5f), Random.Range(-1f, 5f), Random.Range(-5f, 5f));
+        positionArray[1] = new Vector3(Random.Range(-5f, 5f), Random.Range(0, 5f), Random.Range(-5f, 5f));
+        positionArray[2] = new Vector3(Random.Range(-5f, 5f), Random.Range(-2f, 5f), Random.Range(-5f, 5f));
+        positionArray[3] = new Vector3(Random.Range(-5f, 5f), Random.Range(-1f, 5f), Random.Range(-5f, 5f));
+        BezierPath bezierPath = new BezierPath(positionArray, false, PathSpace.xyz);
+        path = new VertexPath(bezierPath);
+
+        ducky.AddComponent<DuckMovement>();
+        DuckMovement pathy = ducky.GetComponent<DuckMovement>();
+        pathy.path = path;
+        Instantiate(ducky, transform.position, transform.rotation);
+        
+        
+
+
+    }
+}
