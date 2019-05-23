8cd52299c2dd5ba4c592b58a9b6dd5a874d2dbda
Paweł Dziedzic <blavikenn@gmail.com>
2019-05-23 13:28:40 +0200

    Change path creation method

--- DHVRv2/Assets/_Scripts/DuckSpawner.cs
+++ DHVRv2/Assets/_Scripts/DuckSpawner.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class DuckSpawner : MonoBehaviour {
     public DuckController _duckPrefab;
     public Vector3 _duckMovementBox;
+    public float _pathDistance;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            Spawn(4, 10);
+        }
+    }
     public DuckController Spawn(float duckSpeed, int controlPointsCount) {
         //Generating duck path
-        var positionArray = new Vector3[controlPointsCount];
-        for (int i = 0; i < controlPointsCount; i++) {
-            positionArray[i] = new Vector3(Random.Range(-_duckMovementBox.x, _duckMovementBox.x),
-                Random.Range(-_duckMovementBox.y, _duckMovementBox.y),
-                Random.Range(-_duckMovementBox.z, _duckMovementBox.z)) + transform.position;
-        }
+        var positionArray = CreatePath(controlPointsCount);
 
         BezierPath bezierPath = new BezierPath(positionArray, false, PathSpace.xyz);
         var path = new VertexPath(bezierPath);
@@ -20,7 +22,36 @@ public class DuckSpawner : MonoBehaviour {
         var duck = Instantiate(_duckPrefab, positionArray[0], Quaternion.identity);
         duck.Initialize(duckSpeed, path);
 
+        //Debug.Log(path.cumulativeLengthAtEachVertex[path.NumVertices - 1]);
+
         return duck;
+    }
+
+    Vector3[] CreatePath(int pathLength) {
+        var path = new Vector3[pathLength];
+        Bounds box = new Bounds(transform.position, _duckMovementBox);
+        var segmentLength = _pathDistance / pathLength;
+
+        // random starting point
+        path[0] = new Vector3(Random.Range(-_duckMovementBox.x, _duckMovementBox.x),
+            Random.Range(-_duckMovementBox.y, _duckMovementBox.y),
+            Random.Range(-_duckMovementBox.z, _duckMovementBox.z)) / 2f + transform.position;
+
+        for (int i = 1; i < pathLength; i++) {
+            // find next direction
+            var dir = Random.onUnitSphere;
+
+            // check it until it lands inside box
+            var point = path[i - 1] + dir * segmentLength;
+            while (!box.Contains(point)) {
+                dir = Random.onUnitSphere;
+                point = path[i - 1] + dir * segmentLength;
+            }
+
+            path[i] = point;
+        }
+
+        return path;
     }
 
     private void OnDrawGizmosSelected() {
